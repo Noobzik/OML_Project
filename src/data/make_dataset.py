81a6d93faee6b89e6f31f57a9ac24d03570f5507
NoobZik <noobzik@pm.me>
2022-07-06 11:56:26 +0200

    Making dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,17 +3,28 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import pandas as pd
 
+# @click.command()
+# @click.argument('input_filepath', type=click.Path(exists=True))
+# @click.argument('output_filepath', type=click.Path())
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    X = pd.read_csv("../../data/raw/NHANESI_X.csv")
+    Y = pd.read_csv("../../data/raw/NHANESI_Y.csv")
+    df = pd.merge(X, Y, on=['Unnamed: 0']).rename(columns={'Unnamed: 0': "ID"})
+    df['date'] = pd.date_range(start='1/1/2019', periods=len(df), freq='H')
+    newdf = pd.to_datetime(df['date']).dt.normalize()
+    df['date'] = newdf
+
+    df.to_csv("../../data/processed/NHANESI_with_temporal.csv", index=False)
+
+
 
 
 if __name__ == '__main__':
